51659017339a219e50938e03cd263d2513ed703c
Lisa Ryd-Carlsson <117908955+LisaRydCarlsson@users.noreply.github.com>
2024-03-16 09:45:48 +0100

    Added Routes, pages, componenets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,54 @@
-import React from "react";
+// components/Header.jsx
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styling/Header.scss";
 
 const Header = () => {
+	const [activePath, setActivePath] = useState("");
+
+	const handleLinkClick = (path) => {
+		setActivePath(path);
+	};
+
 	return (
 		<header>
-			<nav>
+			<nav className="header__nav">
 				<ul>
 					<li>
-						<Link to="/">HOME</Link>
+						<Link
+							to="/"
+							onClick={() => handleLinkClick("/")}
+							className={activePath === "/" ? "clicked" : ""}
+						>
+							HOME
+						</Link>
 					</li>
 					<li>
-						<Link to="/projects">PROJECTS</Link>
+						<Link
+							to="/projects"
+							onClick={() => handleLinkClick("/projects")}
+							className={activePath === "/projects" ? "clicked" : ""}
+						>
+							PROJECTS
+						</Link>
 					</li>
 					<li>
-						<Link to="/about">ABOUT</Link>
+						<Link
+							to="/about"
+							onClick={() => handleLinkClick("/about")}
+							className={activePath === "/about" ? "clicked" : ""}
+						>
+							ABOUT
+						</Link>
 					</li>
 					<li>
-						<Link to="/contact">CONTACT</Link>
+						<Link
+							to="/contact"
+							onClick={() => handleLinkClick("/contact")}
+							className={activePath === "/contact" ? "clicked" : ""}
+						>
+							CONTACT
+						</Link>
 					</li>
 				</ul>
 			</nav>
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,22 @@
+// pages/ContactPage
+import "../styling/HomePage.scss";
+
+function ContactPage() {
+	return (
+		<>
+			<header></header>
+			<main className="contact">
+				<h1>Reach out and let’s explore possibilities together!</h1>
+				<section className="contact__container">
+					<p className="contact__company-p">LONG STORY SHORT AB</p>
+					<p className="contact__info-p">Lisa Ryd-Carlsson</p>
+					<p className="contact__info-p">Nymilsgatan 27</p>
+					<p className="contact__info-p">421 70 Västra Frölunda</p>
+				</section>
+			</main>
+			<footer className="footer-orange"></footer>
+		</>
+	);
+}
+
+export default ContactPage;
